58fc1ec39382994176f41a6ffe776a1b8f4054de
Faisal-rio <faisal.sfzubaida@gmail.com>
2024-09-24 11:44:09 -0700

    commit

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -41,7 +41,7 @@ const ContactUs = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/contact",
+        "https://lms-last-backend.onrender.com/api/contact",
         formData
       );
       console.log("Response:", response.data);
